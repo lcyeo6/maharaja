493e821ca128d8e8e0863a80fc57c8235a1ab656
Lin Chung <lcyeo6@student.monash.edu>
2019-05-09 20:32:45 +0800

    fized bugs in balanced

--- SVM-based Sentiment Analysis/svm_overfit.py
+++ SVM-based Sentiment Analysis/svm_overfit.py
@@ -23,6 +23,7 @@ from sklearn.svm import SVC
 from sklearn import metrics
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from sklearn.model_selection import KFold
+from imblearn.over_sampling import ADASYN
 
 def read_excel(filename):
     
@@ -75,96 +76,6 @@ def pre_process(dataset):
 
     return dataset
 
-def class_equity(a, b):
-    # Count the frequency of each category
-    occurrence = Counter(b)
-    
-    # The least common category will be the minimum equity
-    maximum = occurrence.most_common()[-1][1]
-    print(occurrence)
-    print(maximum)
-
-    # Serve as counter
-    total = dict()
-    for category in occurrence.keys():
-        total[category] = 0
-
-    equalized_a = []
-    equalized_b = []
-    no_of_words = {"50":0, "100":0, "150":0, "200":0, "250":0, "300":0}
-    counter_1 = {"50":0, "100":0, "150":0, "200":0, "250":0, "300":0}
-    counter_2 = {"50":0, "100":0, "150":0, "200":0, "250":0, "300":0}
-    counter_3 = {"50":0, "100":0, "150":0, "200":0, "250":0, "300":0}
-    counter_4 = {"50":0, "100":0, "150":0, "200":0, "250":0, "300":0}
-    counter_5 = {"50":0, "100":0, "150":0, "200":0, "250":0, "300":0}
-    for i, e in enumerate(b):
-        if e == 1:
-            if len(a[i]) <= 50:
-                no_of_words["50"] +=1
-            elif len(a[i]) >50 and len(a[i]) <=100:
-                no_of_words["100"] +=1
-            elif len(a[i]) >100 and len(a[i]) <=150:
-                no_of_words["150"] +=1
-            elif len(a[i]) >150 and len(a[i]) <=200:
-                no_of_words["200"] +=1
-            elif len(a[i]) >200 and len(a[i]) <=250:
-                no_of_words["250"] +=1
-            else:
-                no_of_words["300"] +=1         
-        
-    # Balance the dataset by removing over-represented samples from two arrays
-    def equalizer(counter):
-        if total[element] < maximum:
-            if len(a[index]) <= 50:
-                if counter["50"] < no_of_words["50"]:
-                    counter["50"] +=1
-                    equalized_a.append(a[index])
-                    equalized_b.append(element)
-                    total[element] += 1
-            elif len(a[index]) > 50 and len(a[index]) <= 100:
-                if counter["100"] < no_of_words["100"]:
-                    counter["100"] +=1
-                    equalized_a.append(a[index])
-                    equalized_b.append(element)
-                    total[element] += 1
-            elif len(a[index]) > 100 and len(a[index]) <= 150:
-                if counter["150"] < no_of_words["150"]:
-                    counter["150"] +=1
-                    equalized_a.append(a[index])
-                    equalized_b.append(element)
-                    total[element] += 1
-            elif len(a[index]) > 150 and len(a[index]) <= 200:
-                if counter["200"] < no_of_words["200"]:
-                    counter["200"] +=1
-                    equalized_a.append(a[index])
-                    equalized_b.append(element)
-                    total[element] += 1
-            elif len(a[index]) > 200 and len(a[index]) <= 250:
-                if counter["250"] < no_of_words["250"]:
-                    counter["250"] +=1
-                    equalized_a.append(a[index])
-                    equalized_b.append(element)
-                    total[element] += 1
-            else:
-                if counter["300"] < no_of_words["300"]:
-                    counter["300"] +=1
-                    equalized_a.append(a[index])
-                    equalized_b.append(element)
-                    total[element] += 1
-                    
-    for index, element in enumerate(b):
-        if element == 1:
-            equalizer(counter_1)
-        elif element == 2:
-            equalizer(counter_2)
-        elif element == 3:
-            equalizer(counter_3)
-        elif element == 4:
-            equalizer(counter_4)
-        else:
-            equalizer(counter_5)
-
-    return equalized_a, equalized_b
 
 def identify_token(text):
 #   Return it's text back, as requested as a token
@@ -189,18 +100,17 @@ for index, row in filtered_dataset.iterrows():
     review_text.append(row[7])
     star_rating.append(int(row[3][0]))
     
-equalized_review_text, equalized_star_rating = class_equity(review_text, star_rating)
 print("PRE-process time")
 print(datetime.datetime.now() - t1)
 
-
+ 
 # Vectorize review text into unigram, bigram and evaluates into a term document matrix of TF-IDF features
-tfidf_vectorizer = TfidfVectorizer(tokenizer=identify_token, ngram_range = (1, 1), lowercase=False)
+tfidf_vectorizer = TfidfVectorizer(tokenizer=identify_token, ngram_range = (1, 2), lowercase=False)
 t2 = datetime.datetime.now()
 
 # Construct vocabulary and inverse document frequency from all the review texts
 # Then, transform each review text into a tf-idf weighted document term matrix
-vectorized_data = tfidf_vectorizer.fit_transform(equalized_review_text)
+vectorized_data = tfidf_vectorizer.fit_transform(review_text)
 #print(vectorized_data)
 print("Vectorizing time")
 print(datetime.datetime.now() - t2)
@@ -216,12 +126,14 @@ print(datetime.datetime.now() - t2)
 #    print (("Mean score: {0:.3f} (+/-{1:.3f})").format(np.mean(scores), sem(scores)))
 
 def train_and_evaluate(clf, X_train, X_test, y_train, y_test, accuracy_train, accuracy_test, precision, recall, f1):
+    sm = ADASYN()
+    overfit_X, overfit_y = sm.fit_sample(X_train, y_train)
     # Function to perform training on the training set and evaluate the performance on the testing set
-    clf.fit(X_train, y_train)
+    clf.fit(overfit_X, overfit_y)
     
 #    print ("Accuracy on training set:")
 #    print (clf.score(X_train, y_train))
-    accuracy_train.append(clf.score(X_train, y_train))
+    accuracy_train.append(clf.score(overfit_X, overfit_y))
 #    print ("Accuracy on testing set:")
 #    print (clf.score(X_test, y_test))
     accuracy_test.append(clf.score(X_test, y_test))
@@ -247,21 +159,21 @@ precision = []
 recall = []
 f1 = []
 
-# Split dataset into training and testing
 n=0
+# Split dataset into training and testing
+
 kfold = KFold(10, True, 1)
-for train_index, test_index in kfold.split(vectorized_data, equalized_star_rating):
-    n+=1
+for train_index, test_index in kfold.split(vectorized_data, star_rating):
+    n +=1
     print(n)
     t3 = datetime.datetime.now()
     X_train, X_test = vectorized_data[train_index], vectorized_data[test_index]
-    y_train = [equalized_star_rating[i] for i in train_index]
-    y_test = [equalized_star_rating[i] for i in test_index]
+    y_train = [star_rating[i] for i in train_index]
+    y_test = [star_rating[i] for i in test_index]
     train_and_evaluate(svc_1, X_train, X_test, y_train, y_test, accuracy_train, accuracy_test, precision, recall, f1)
     print("Train and test time")
     print(datetime.datetime.now() - t3)
-    
-    
+
 print("accuracy_train: {}".format(np.mean(accuracy_train)))
 print("accuracy_test: {}".format(np.mean(accuracy_test)))
 print("precision: {}".format(np.mean(precision)))
