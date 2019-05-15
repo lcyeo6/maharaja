5c7a703225bf54f7ec67a1e5e315fd47081ecd65
Kris Wan <wct_1505@hotmail.com>
2019-05-15 20:29:44 +0800

    Added svm_balanced_combined

--- SVM-based Sentiment Analysis/svm_balanced_combined.py
+++ SVM-based Sentiment Analysis/svm_balanced_combined.py
@@ -80,7 +80,7 @@ def class_equity(a, b):
     occurrence = Counter(b)
     
     # The least common category will be the minimum equity
-    maximum_star_rating = occurrence.most_common()[-1][0]
+    maximum_sentiment = occurrence.most_common()[-1][0]
     maximum_amount = occurrence.most_common()[-1][1]
     
     # Serve as counter
@@ -95,8 +95,8 @@ def class_equity(a, b):
     no_of_words = {"50": 0, "100": 0, "150": 0, "200": 0, "250": 0, "300": 0}
 	
 	# To check the number of words of the review text with the least amount of star rating
-    for i, star_rating in enumerate(b):
-        if star_rating == maximum_star_rating:
+    for i, sentiment in enumerate(b):
+        if sentiment == maximum_sentiment:
             if len(a[i]) <= 50:
                 no_of_words["50"] += 1
                 
@@ -116,76 +116,61 @@ def class_equity(a, b):
                 no_of_words["300"] += 1  
                 
     # Counter to keep track of the number of words of the review text with respective star rating
-    counter_1 = {"50": 0, "100": 0, "150": 0, "200": 0, "250": 0, "300": 0}
-    counter_2 = {"50": 0, "100": 0, "150": 0, "200": 0, "250": 0, "300": 0}
-    counter_3 = {"50": 0, "100": 0, "150": 0, "200": 0, "250": 0, "300": 0}
-    counter_4 = {"50": 0, "100": 0, "150": 0, "200": 0, "250": 0, "300": 0}
-    counter_5 = {"50": 0, "100": 0, "150": 0, "200": 0, "250": 0, "300": 0}
+    counter_positive = {"50": 0, "100": 0, "150": 0, "200": 0, "250": 0, "300": 0}
+    counter_negative = {"50": 0, "100": 0, "150": 0, "200": 0, "250": 0, "300": 0}
     
     def equalizer(counter):
         
         # Balance the dataset by removing over-represented samples from two arrays
-        if total[star_rating] < maximum_amount:
+        if total[sentiment] < maximum_amount:
             if len(a[index]) <= 50:
                 if counter["50"] < no_of_words["50"]:
                     counter["50"] += 1
                     equalized_a.append(a[index])
-                    equalized_b.append(star_rating)
-                    total[star_rating] += 1
+                    equalized_b.append(sentiment)
+                    total[sentiment] += 1
                     
             elif len(a[index]) > 50 and len(a[index]) <= 100:
                 if counter["100"] < no_of_words["100"]:
                     counter["100"] += 1
                     equalized_a.append(a[index])
-                    equalized_b.append(star_rating)
-                    total[star_rating] += 1
+                    equalized_b.append(sentiment)
+                    total[sentiment] += 1
                     
             elif len(a[index]) > 100 and len(a[index]) <= 150:
                 if counter["150"] < no_of_words["150"]:
                     counter["150"] += 1
                     equalized_a.append(a[index])
-                    equalized_b.append(star_rating)
-                    total[star_rating] += 1
+                    equalized_b.append(sentiment)
+                    total[sentiment] += 1
                     
             elif len(a[index]) > 150 and len(a[index]) <= 200:
                 if counter["200"] < no_of_words["200"]:
                     counter["200"] += 1
                     equalized_a.append(a[index])
-                    equalized_b.append(star_rating)
-                    total[star_rating] += 1
+                    equalized_b.append(sentiment)
+                    total[sentiment] += 1
                     
             elif len(a[index]) > 200 and len(a[index]) <= 250:
                 if counter["250"] < no_of_words["250"]:
                     counter["250"] += 1
                     equalized_a.append(a[index])
-                    equalized_b.append(star_rating)
-                    total[star_rating] += 1
+                    equalized_b.append(sentiment)
+                    total[sentiment] += 1
                     
             else:
                 if counter["300"] < no_of_words["300"]:
                     counter["300"] += 1
                     equalized_a.append(a[index])
-                    equalized_b.append(star_rating)
-                    total[star_rating] += 1
+                    equalized_b.append(sentiment)
+                    total[sentiment] += 1
                     
 	# Balance data for each star rating
-    for index, star_rating in enumerate(b):
-        if star_rating == 1:
-            equalizer(counter_1)
-        elif star_rating == 2:
-            equalizer(counter_2)
-        elif star_rating == 3:
-            equalizer(counter_3)
-        elif star_rating == 4:
-            equalizer(counter_4)
-        else:
-            equalizer(counter_5)
-            
-    print(counter_1)
-    print(counter_2)
-    print(counter_3)
-    print(counter_4)
-    print(counter_5)
+    for index, sentiment in enumerate(b):
+        if sentiment == "positive":
+            equalizer(counter_positive)
+        elif sentiment == "negative":
+            equalizer(counter_negative)
 
     return equalized_a, equalized_b
 
@@ -193,6 +178,17 @@ def identity_token(text):
     
     # Return it's text back, as requested as a token
     return text
+
+def actual_sentiment(data):
+    
+    # Convert star-rating into respective sentiments
+    for i in data:
+        if int(i[0]) == 4 or int(i[0]) == 5:
+            return "positive"
+        elif int(i[0]) == 1 or int(i[0]) == 2:
+            return "negative"
+        else:
+            return "neutral"
 
 "--------------------------------------------------------------------------------------------------------------------"
 
@@ -204,6 +200,10 @@ t1 = datetime.datetime.now()
 # Data cleaning & pre-processing
 filtered_dataset = pre_process(dataset)
 
+filtered_dataset["actual_sentiment"] = filtered_dataset.rating.apply(actual_sentiment)
+
+filtered_dataset = filtered_dataset[filtered_dataset.actual_sentiment != "neutral"]
+
 print("Pre-process Time")
 print(datetime.datetime.now() - t1)
 print()
@@ -212,13 +212,13 @@ print()
 
 # Storing both normalized review texts and star ratings in repective arrays
 review_text = []
-star_rating = []
+sentiment = []
 for index, row in filtered_dataset.iterrows():
     review_text.append(row[7])
-    star_rating.append(int(row[3][0]))
+    sentiment.append(row[8])
 
 # Balance the data we are going to put into training and testing
-equalized_review_text, equalized_star_rating = class_equity(review_text, star_rating)
+equalized_review_text, equalized_sentiment = class_equity(review_text, sentiment)
 
 # Vectorize review text into unigram, bigram and evaluates into a term document matrix of TF-IDF features
 tfidf_vectorizer = TfidfVectorizer(tokenizer = identity_token, ngram_range = (1, 1), lowercase = False)
@@ -298,13 +298,13 @@ n = 0
 
 # Split dataset into training and testing, using 10-fold classification
 kfold = KFold(10, True, 1)
-for train_index, test_index in kfold.split(vectorized_data, equalized_star_rating):
+for train_index, test_index in kfold.split(vectorized_data, equalized_sentiment):
     n += 1
     print(n)
     t3 = datetime.datetime.now()
     X_train, X_test = vectorized_data[train_index], vectorized_data[test_index]
-    y_train = [equalized_star_rating[i] for i in train_index]
-    y_test = [equalized_star_rating[i] for i in test_index]
+    y_train = [equalized_sentiment[i] for i in train_index]
+    y_test = [equalized_sentiment[i] for i in test_index]
 	
 	# Train and test the data
     train_and_evaluate(svc, X_train, X_test, y_train, y_test, accuracy_train, accuracy_test, precision_micro, recall_micro, f1_micro, precision_macro, recall_macro, f1_macro, precision_weight, recall_weight, f1_weight)
